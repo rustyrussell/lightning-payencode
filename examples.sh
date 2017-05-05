ccaf48f67d9f9e2413686ef5c7a6f239107d5f6c
Rusty Russell <rusty@rustcorp.com.au>
2017-05-05 17:01:08 +0930

    README.sh: fix examples, remove finished TODO about signatures.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- examples.sh
+++ examples.sh
@@ -7,22 +7,22 @@ CONVERSION_RATE=1200
 PRIVKEY=e126f68f7eafcc8b74f54d269fe206be715000f94dac067d1c04a8ca3b2db734
 PUBKEY=03e7156ae33b0a208d0744199163177e909e80176e55d97a2f221ede0f934dd9ad
 
-echo "# Please send 10 satoshi using rhash $RHASH to me @$PUBKEY"
+echo "### Please send 10 satoshi using rhash $RHASH to me @$PUBKEY"
 ./lightning-address.py encode 10000 $RHASH $PRIVKEY
 echo
 
-echo "# Please send \$3 for a cup of coffee to the same peer, within 1 minute"
+echo "### Please send \$3 for a cup of coffee to the same peer, within 1 minute"
 ./lightning-address.py encode --description='1 cup coffee' $((3 * 100000000000 / $CONVERSION_RATE)) --expires=60 $RHASH $PRIVKEY
 echo
 
-echo "# Now send \$24 for an entire list of things (hashed)"
+echo "### Now send \$24 for an entire list of things (hashed)"
 ./lightning-address.py encode --description-hashed='One piece of chocolate cake, one icecream cone, one pickle, one slice of swiss cheese, one slice of salami, one lollypop, one piece of cherry pie, one sausage, one cupcake, and one slice of watermelon' $((24 * 100000000000 / $CONVERSION_RATE)) $RHASH $PRIVKEY
 echo
 
 # NOTE: Does not implement real fallback format yet!
-echo '# The same, with a fallback address mk2QpYatsKicvFVuTAQLBryyccRXMUaGHP'
+echo '### The same, with a fallback address mk2QpYatsKicvFVuTAQLBryyccRXMUaGHP'
 ./lightning-address.py encode --fallback=mk2QpYatsKicvFVuTAQLBryyccRXMUaGHP --description-hashed='One piece of chocolate cake, one icecream cone, one pickle, one slice of swiss cheese, one slice of salami, one lollypop, one piece of cherry pie, one sausage, one cupcake, and one slice of watermelon' $((24 * 100000000000 / $CONVERSION_RATE)) $RHASH $PRIVKEY
 echo
 
-echo '# The same, with extra routing info to get to node 029e03a901b85534ff1e92c43c74431f7ce72046060fcf7a95c37e148f78c77255'
+echo '### The same, with extra routing info to get to node 029e03a901b85534ff1e92c43c74431f7ce72046060fcf7a95c37e148f78c77255'
 ./lightning-address.py encode --route=029e03a901b85534ff1e92c43c74431f7ce72046060fcf7a95c37e148f78c77255/0102030405060708/20/3 --fallback=mk2QpYatsKicvFVuTAQLBryyccRXMUaGHP --description-hashed='One piece of chocolate cake, one icecream cone, one pickle, one slice of swiss cheese, one slice of salami, one lollypop, one piece of cherry pie, one sausage, one cupcake, and one slice of watermelon' $((24 * 100000000000 / $CONVERSION_RATE)) $RHASH $PRIVKEY
