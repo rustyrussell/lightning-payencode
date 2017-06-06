b2b4f9755b6659dceea6518dfe6c52ee28aa6ae0
Christian Decker <decker.christian@gmail.com>
2017-06-06 16:13:24 +0200

    Split main entrypoint from library functions

---
+++ lightning-address.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python3
+from lnaddr import lnencode, lndecode
+
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Encode lightning address')
+subparsers = parser.add_subparsers(dest='subparser_name',
+                                   help='sub-command help')
+
+parser_enc = subparsers.add_parser('encode', help='encode help')
+parser_dec = subparsers.add_parser('decode', help='decode help')
+
+parser_enc.add_argument('--currency', default='bc',
+                    help="What currency")
+parser_enc.add_argument('--route', action='append', default=[],
+                        help="Extra route steps of form pubkey/channel/fee/cltv")
+parser_enc.add_argument('--fallback',
+                        help='Fallback address for onchain payment')
+parser_enc.add_argument('--description',
+                        help='What is being purchased')
+parser_enc.add_argument('--description-hashed',
+                        help='What is being purchased (for hashing)')
+parser_enc.add_argument('--expires', type=int,
+                        help='Seconds before offer expires')
+parser_enc.add_argument('--no-amount', action="store_true",
+                        help="Don't encode amount")
+parser_enc.add_argument('amount', type=float, help='Amount in currency')
+parser_enc.add_argument('paymenthash', help='Payment hash (in hex)')
+parser_enc.add_argument('privkey', help='Private key (in hex)')
+parser_enc.set_defaults(func=lnencode)
+
+parser_dec.add_argument('lnaddress', help='Address to decode')
+parser_dec.add_argument('--rate', type=float, help='Convfersion amount for 1 currency unit')
+parser_dec.add_argument('--pubkey', help='Public key for the chanid')
+parser_dec.set_defaults(func=lndecode)
+
+if __name__ == "__main__":
+    options = parser.parse_args()
+    if not options.subparser_name:
+        parser.print_help()
+    else:
+        options.func(options)
--- lnaddr.py
+++ lnaddr.py
@@ -2,7 +2,6 @@
 from bech32 import bech32_encode, bech32_decode, convertbits, CHARSET
 from decimal import Decimal
 
-import argparse
 import base58
 import hashlib
 import re
@@ -257,41 +256,3 @@ def lndecode(options):
         else:
             tagdata = convertbits(tagdata, 5, 8, False)
             print("UNKNOWN TAG {}: {}".format(tag, bytearray(tagdata).hex()))
-
-
-parser = argparse.ArgumentParser(description='Encode lightning address')
-subparsers = parser.add_subparsers(dest='subparser_name',
-                                   help='sub-command help')
-
-parser_enc = subparsers.add_parser('encode', help='encode help')
-parser_dec = subparsers.add_parser('decode', help='decode help')
-
-parser_enc.add_argument('--currency', default='bc',
-                    help="What currency")
-parser_enc.add_argument('--route', action='append', default=[],
-                        help="Extra route steps of form pubkey/channel/fee/cltv")
-parser_enc.add_argument('--fallback',
-                        help='Fallback address for onchain payment')
-parser_enc.add_argument('--description',
-                        help='What is being purchased')
-parser_enc.add_argument('--description-hashed',
-                        help='What is being purchased (for hashing)')
-parser_enc.add_argument('--expires', type=int,
-                        help='Seconds before offer expires')
-parser_enc.add_argument('--no-amount', action="store_true",
-                        help="Don't encode amount")
-parser_enc.add_argument('amount', type=float, help='Amount in currency')
-parser_enc.add_argument('paymenthash', help='Payment hash (in hex)')
-parser_enc.add_argument('privkey', help='Private key (in hex)')
-parser_enc.set_defaults(func=lnencode)
-
-parser_dec.add_argument('lnaddress', help='Address to decode')
-parser_dec.add_argument('--rate', type=float, help='Convfersion amount for 1 currency unit')
-parser_dec.add_argument('--pubkey', help='Public key for the chanid')
-parser_dec.set_defaults(func=lndecode)
-
-options = parser.parse_args()
-if not options.subparser_name:
-    parser.print_help()
-else:
-    options.func(options)
